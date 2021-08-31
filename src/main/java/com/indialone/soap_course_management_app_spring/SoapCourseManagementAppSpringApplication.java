1e92f765f68c29023272a9b6790c70f58a4808a4
Natavar Prajapati <prajapatinatavar21197@gmail.com>
2021-08-31 08:01:13 +0530

    initial commit

---
+++ src/main/java/com/indialone/soap_course_management_app_spring/SoapCourseManagementAppSpringApplication.java
@@ -0,0 +1,13 @@
+package com.indialone.soap_course_management_app_spring;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SoapCourseManagementAppSpringApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SoapCourseManagementAppSpringApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/indialone/soap_course_management_app_spring/SoapCourseManagementAppSpringApplicationTests.java
@@ -0,0 +1,13 @@
+package com.indialone.soap_course_management_app_spring;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SoapCourseManagementAppSpringApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
